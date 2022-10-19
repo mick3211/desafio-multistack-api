3e855077a1cbc78ffcb5a393f9b19b314fc14814
Mickael <mickaelfelizardo2008@gmail.com>
2022-10-19 18:24:52 -0300

    correção de rotas auth

--- achados_e_perdidos/achados_e_perdidos/urls.py
+++ achados_e_perdidos/achados_e_perdidos/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenBlacklistView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
--- achados_e_perdidos/api/urls.py
+++ achados_e_perdidos/api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
 from .views import (
                     locais_view
                 )
@@ -6,4 +11,7 @@ from .views import (
 urlpatterns = [
     path('locais/', locais_view.LocaisView.as_view(), name='locais-list'),
     path('locais/imagem/', locais_view.ImagemLocalView.as_view(), name='imagem-local'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
