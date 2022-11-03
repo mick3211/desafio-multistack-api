e2f0ad2c13cfd27041575c8fbfb3ca8fddea1351
Mickael <mickaelfelizardo2008@gmail.com>
2022-11-03 14:48:08 -0300

    static files config

--- achados_e_perdidos/achados_e_perdidos/settings.py
+++ achados_e_perdidos/achados_e_perdidos/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from datetime import timedelta
-from pathlib import Path
 import environ
 import os
 
@@ -152,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
